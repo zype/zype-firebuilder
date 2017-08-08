14dcce4a85d94e687c499d1227773ab80d20c947
Eugeny Cherkasov <ech89899@gmail.com>
2017-08-09 00:35:01 +0300

    Fix ad tag macros

--- Application/ZypeAPI/src/main/java/com/zype/fire/api/Util/AdMacrosHelper.java
+++ Application/ZypeAPI/src/main/java/com/zype/fire/api/Util/AdMacrosHelper.java
@@ -7,8 +7,6 @@ import android.net.Uri;
 import android.os.Build;
 import android.provider.Settings;
 
-import java.net.MalformedURLException;
-import java.net.URL;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -21,7 +19,7 @@ public class AdMacrosHelper {
     private static final String APP_DOMAIN = "[app_domain]";
     private static final String APP_ID = "[app_id]";
     private static final String APP_NAME = "[app_name]";
-    private static final String DEVICE_DEVICE_TYPE = "[device_type]";
+    private static final String DEVICE_TYPE = "[device_type]";
     private static final String DEVICE_IFA = "[device_ifa]";
     private static final String DEVICE_MAKE = "[device_make]";
     private static final String DEVICE_MODEL = "[device_model]";
@@ -61,7 +59,7 @@ public class AdMacrosHelper {
         result.put(DEVICE_MODEL, Build.MODEL);
         // Default device type is '7' (set top box device)
         // TODO: Find how to detect device type - 3 (smart TV) or 7 (set top box)
-        result.put(DEVICE_DEVICE_TYPE, "7");
+        result.put(DEVICE_TYPE, "7");
         // Default VPI is 'MP4'
         result.put(VPI, "MP4");
         // UUID us the same as Advertising id
