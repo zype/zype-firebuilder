426a374144811d23c9ee4d8dc37ab339e0b42551
Evgeny Cherkasov <ech89899@gmail.com>
2017-10-06 20:49:43 +0300

    Integration with Zype Builder

--- Application/ZypeAPI/src/demo/java/com/zype/fire/api/ZypeSettings.java
+++ Application/ZypeAPI/src/demo/java/com/zype/fire/api/ZypeSettings.java
@@ -1,19 +1,18 @@
 package com.zype.fire.api;
 
+import android.content.Context;
+
 /**
  * Created by Evgeny Cherkasov on 17.04.2017.
  */
 
 public class ZypeSettings {
     // Zype app key
-    // TODO: Provide your Zype app key
     public static final String APP_KEY = "iBjj-jnjT12tQGmyeOR9op8_RjsGmT1Nq5OEETkbxOXnNmDE1m9MrB0wpti0__9l";
     // OAuth credentials
-    // TODO: Provide your Zype authentication credentials
     public static final String CLIENT_ID = "62f1d247b4c5e77b6111d9a9ed8b3b64bab6be66cc8b7513a928198083cd1c72";
     public static final String CLIENT_SECRET = "06f45687da00bbe3cf51dddc7dbd7a288d1c852cf0b9a6e76e25bb115dcf872c";
     // Playlist
-    // TODO: Provide your Zype root playlist id
     public static final String ROOT_PLAYLIST_ID = "577e65c85577de0d1000c1ee";
 
     // Template version
---
+++ Application/ZypeAPI/src/template/java/com/zype/fire/api/ZypeSettings.java
@@ -0,0 +1,38 @@
+package com.zype.fire.api;
+
+import android.content.Context;
+
+/**
+ * Created by Evgeny Cherkasov on 17.04.2017.
+ */
+
+public class ZypeSettings {
+    // Zype app key
+    public static final String APP_KEY = "[APP_KEY]";
+    // OAuth credentials
+    public static final String CLIENT_ID = "[CLIENT_ID]";
+    public static final String CLIENT_SECRET = "[CLIENT_SECRET]";
+    // Playlist
+    public static final String ROOT_PLAYLIST_ID = "[ROOT_PLAYLIST_ID]";
+
+    // Template version
+    public static final String TEMPLATE_VERSION = "1.1.0";
+
+    // App features
+//    public static final boolean BACKGROUND_PLAYBACK_ENABLED = false;
+//    public static final boolean DOWNLOADS_ENABLED = true;
+//    public static final boolean DOWNLOADS_ENABLED_FOR_GUESTS = true;
+//    public static final boolean SHARE_VIDEO_ENABLED = false;
+//    public static final boolean THEME_LIGHT = true;
+    public static final boolean NATIVE_SUBSCRIPTION_ENABLED = Boolean.valueOf("[NATIVE_SUBSCRIPTION_ENABLED]");
+    public static final boolean NATIVE_TO_UNIVERSAL_SUBSCRIPTION_ENABLED = Boolean.valueOf("[NATIVE_TO_UNIVERSAL_SUBSCRIPTION_ENABLED]");
+    public static final boolean SUBSCRIBE_TO_WATCH_AD_FREE_ENABLED = Boolean.valueOf("[SUBSCRIBE_TO_WATCH_AD_FREE_ENABLED]");
+    public static final boolean UNIVERSAL_SUBSCRIPTION_ENABLED = Boolean.valueOf("[UNIVERSAL_SUBSCRIPTION_ENABLED]");
+
+    /**
+     * Amazon shared key is required for native subscription feature. It is used in request to Zype Bifrost
+     * service for verifying subscription.
+     */
+    public static final String AMAZON_SHARED_KEY = "";
+}
+
