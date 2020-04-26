7d45bf0beb5edeec3a8cc9cb50eebfd0406dfaec
Evgeny Cherkasov <ech89899@gmail.com>
2020-04-26 12:06:42 +0300

    Load terms of services content from zobject

--- TVUIComponent/lib/src/main/java/com/amazon/android/tv/tenfoot/ui/activities/TermsConditionActivity.java
+++ TVUIComponent/lib/src/main/java/com/amazon/android/tv/tenfoot/ui/activities/TermsConditionActivity.java
@@ -7,12 +7,15 @@ import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
 import com.amazon.android.tv.tenfoot.R;
+import com.amazon.android.utils.Preferences;
 import com.zype.fire.api.ZypeSettings;
 
 /**
  * A class to display content in a vertical grid.
  */
 public class TermsConditionActivity extends Activity {
+
+    private static final String PREFERENCE_TERMS = "ZypeTerms";
 
     /**
      * Called when the activity is first created.
@@ -24,7 +27,8 @@ public class TermsConditionActivity extends Activity {
         WebView webView = (WebView) findViewById(R.id.webView);
 
         webView.getSettings().setJavaScriptEnabled(true);
-        webView.loadUrl(ZypeSettings.TERMS_CONDITION_URL);
+//        webView.loadUrl(ZypeSettings.TERMS_CONDITION_URL);
+        webView.loadData(Preferences.getString(PREFERENCE_TERMS), "text/html", "UTF-8");
 
         webView.setWebViewClient(new WebViewClient() {
 
