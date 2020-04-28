56857552f735ed0e8ed3f3661c35daf888dd8b86
Evgeny Cherkasov <ech89899@gmail.com>
2020-04-29 01:19:04 +0300

    Update Terms view (APPS-855) (#67)
    * Load terms of services content from zobject
    
    * Make background and text color on Terms screen consistent with app theme

--- TVUIComponent/lib/src/main/java/com/amazon/android/tv/tenfoot/ui/activities/TermsConditionActivity.java
+++ TVUIComponent/lib/src/main/java/com/amazon/android/tv/tenfoot/ui/activities/TermsConditionActivity.java
@@ -2,17 +2,21 @@ package com.amazon.android.tv.tenfoot.ui.activities;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
 import android.view.View;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
 import com.amazon.android.tv.tenfoot.R;
+import com.amazon.android.utils.Preferences;
 import com.zype.fire.api.ZypeSettings;
 
 /**
  * A class to display content in a vertical grid.
  */
 public class TermsConditionActivity extends Activity {
+
+    private static final String PREFERENCE_TERMS = "ZypeTerms";
 
     /**
      * Called when the activity is first created.
@@ -24,7 +28,18 @@ public class TermsConditionActivity extends Activity {
         WebView webView = (WebView) findViewById(R.id.webView);
 
         webView.getSettings().setJavaScriptEnabled(true);
-        webView.loadUrl(ZypeSettings.TERMS_CONDITION_URL);
+        webView.getSettings().setLoadWithOverviewMode(true);
+        webView.getSettings().setUseWideViewPort(true);
+        webView.setBackgroundColor(ContextCompat.getColor(this, R.color.browse_background_color));
+//        webView.loadUrl(ZypeSettings.TERMS_CONDITION_URL);
+        String textColor = ZypeSettings.LIGHT_THEME ? "#000" : "#fff";
+        String finalHtml = "<html><head>"
+                + "<style type=\"text/css\">body {color: " + textColor + "} "
+                + "</style></head>"
+                + "<body>"
+                + Preferences.getString(PREFERENCE_TERMS)
+                + "</body></html>";
+        webView.loadData(finalHtml, "text/html", "UTF-8");
 
         webView.setWebViewClient(new WebViewClient() {
 
