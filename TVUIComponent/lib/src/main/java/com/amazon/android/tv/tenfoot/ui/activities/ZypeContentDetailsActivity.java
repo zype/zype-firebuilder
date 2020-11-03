9fd1a32c21ba3cd4679ceb2fd8f8746a2fffb3f7
anurag <anuragdce@gmail.com>
2020-11-03 17:55:02 +0530

    light theme color fixes.

--- TVUIComponent/lib/src/main/java/com/amazon/android/tv/tenfoot/ui/activities/ZypeContentDetailsActivity.java
+++ TVUIComponent/lib/src/main/java/com/amazon/android/tv/tenfoot/ui/activities/ZypeContentDetailsActivity.java
@@ -484,7 +484,7 @@ public class ZypeContentDetailsActivity extends BaseActivity
         MenuFragment fragment = (MenuFragment) getFragmentManager().findFragmentById(R.id.fragmentMenu);
         if (fragment != null) {
             isMenuOpened = true;
-            fragment.getView().setBackgroundColor(ContextCompat.getColor(this, R.color.lb_error_background_color_translucent));
+            fragment.getView().setBackgroundColor(ContextCompat.getColor(this, R.color.background_color_translucent));
 //             fragment.getView().setBackgroundColor(ContextCompat.getColor(this, R.color.left_menu_background));
             int paddingTop = (int) getResources().getDimension(R.dimen.lb_browse_padding_top);
             fragment.getView().setPadding(0, paddingTop, 0, 0);
